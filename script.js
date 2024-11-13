0bebaaefeff01be55a39a768fa75d946d512415d
paschalugwu <ugwupaschal@gmail.com>
2024-11-13 08:55:24 +0100

    Changes to services and projects rows

--- script.js
+++ script.js
@@ -7,13 +7,28 @@ hamburger.addEventListener('click', function () {
     const hamIcon = this.querySelector('.hamburger-icon'); 
     const crossIcon = this.querySelector('.cross-icon'); 
     if (hamIcon.style.display === "none") { 
-        hamIcon.style.display = "inline-block"
-        menu.style.display = "none"
-        crossIcon.style.display = "none"
-    } 
-    else { 
-        crossIcon.style.display = "inline-block"
-        hamIcon.style.display = "none"
-        menu.style.display = "block"
+        hamIcon.style.display = "inline-block";
+        menu.style.display = "none";
+        crossIcon.style.display = "none";
+    } else { 
+        crossIcon.style.display = "inline-block";
+        hamIcon.style.display = "none";
+        menu.style.display = "block";
     } 
 });
+
+function scrollLeft() {
+    document.querySelector('.services-content').scrollBy({
+        top: 0,
+        left: -300, // Adjust this value based on the width of the cards
+        behavior: 'smooth'
+    });
+}
+
+function scrollRight() {
+    document.querySelector('.services-content').scrollBy({
+        top: 0,
+        left: 300, // Adjust this value based on the width of the cards
+        behavior: 'smooth'
+    });
+}
